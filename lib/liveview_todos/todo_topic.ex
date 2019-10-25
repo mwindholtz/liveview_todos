835588c0f1d258624dc4796191207d4277d3cb6c
Mark Windholtz <mark@agiledna.com>
2019-10-24 20:51:50 -0400

    adds topic

---
+++ lib/liveview_todos/todo_topic.ex
@@ -0,0 +1,21 @@
+defmodule LiveviewTodos.TodoTopic do
+  @moduledoc """
+  The TodoTopic
+  """
+
+  @topic inspect(__MODULE__)
+
+  def subscribe do
+    Phoenix.PubSub.subscribe(LiveviewTodos.PubSub, @topic)
+  end
+
+  def broadcast_change({:ok, result}, event) do
+    Phoenix.PubSub.broadcast(LiveviewTodos.PubSub, @topic, {__MODULE__, event, result})
+    {:ok, result}
+  end
+
+  def broadcast_change({:error, changeset}, event) do
+    Phoenix.PubSub.broadcast(LiveviewTodos.PubSub, @topic, {__MODULE__, event, :error, changeset})
+    {:error, changeset}
+  end
+end
