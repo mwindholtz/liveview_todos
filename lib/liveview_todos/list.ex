4d071ef02c8a762b128b6fe7ae88b62d008f8d06
Mark Windholtz <mark@agiledna.com>
2019-10-28 18:50:13 -0400

    limbo-commit

--- lib/liveview_todos/list.ex
+++ lib/liveview_todos/list.ex
@@ -1,5 +1,12 @@
 defmodule LiveviewTodos.List do
   @moduledoc """
+  List is the Aggregate in our example app.
+  It guarantees invarients.
+  (1) When all items are done, the list should be marked as done
+  (2) Each list has a WIP (Work in Process) Limit of 5 items, no more may be added
+
+  And it does not reveal the details of the contained Entities (the Items).
+  So no item ids will leak out of this Aggregate.
   """
   use Ecto.Schema
   import Ecto.Changeset
