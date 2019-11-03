e913dfbd58bd672f9f3ace97350937f5a873577f
Mark Windholtz <mark@agiledna.com>
2019-11-03 17:32:19 -0500

    better test shutdown

--- test/liveview_todos/todo_application_service_test.exs
+++ test/liveview_todos/todo_application_service_test.exs
@@ -4,15 +4,21 @@ defmodule LiveviewTodos.TodoApplicationServiceTest do
   alias LiveviewTodos.TodoApplicationService, as: Service
   alias LiveviewTodos.List
 
+  setup do
+    LiveviewTodos.TodoTopic.subscribe()
+    :ok
+  end
+
   describe "item" do
     setup do
       {:ok, list} = Service.create_list("Homework")
+      assert_receive {LiveviewTodos.TodoTopic, [:lists, :created], new_list}
+
       attrs = %{"description" => "description", "list_id" => list.id}
       %{attrs: attrs}
     end
 
     test "get_item!/2", %{attrs: attrs} do
-      LiveviewTodos.TodoTopic.subscribe()
       :ok = Service.create_item(attrs)
 
       assert_receive {LiveviewTodos.TodoTopic, [:todo, :created], todo}
@@ -20,12 +26,8 @@ defmodule LiveviewTodos.TodoApplicationServiceTest do
     end
 
     test "create_item/1", %{attrs: attrs} do
-      #      assert_repo_changed(Todo, 1, fn ->
       assert :ok = Service.create_item(attrs)
-      # assert todo.title == "description"
-      # assert todo.done == false
-      #     end)
-      Process.sleep(200)
+      assert_receive {LiveviewTodos.TodoTopic, [:todo, :created], todo}
     end
   end
 
@@ -33,6 +35,7 @@ defmodule LiveviewTodos.TodoApplicationServiceTest do
     setup do
       name_of_list = "Grocery"
       {:ok, list} = Service.create_list(name_of_list)
+      assert_receive {LiveviewTodos.TodoTopic, [:lists, :created], new_list}
 
       %{list: list, name_of_list: name_of_list}
     end
@@ -40,19 +43,14 @@ defmodule LiveviewTodos.TodoApplicationServiceTest do
     test "create_list/1" do
       name_of_list = "School"
 
-      assert_repo_changed(List, 1, fn ->
-        result = Service.create_list(name_of_list)
-        assert {:ok, %List{} = list} = result
-        assert list.name == name_of_list
-      end)
+      Service.create_list(name_of_list)
+      assert_receive {LiveviewTodos.TodoTopic, [:lists, :created], new_list}
     end
 
     test "delete_list/1",
          %{list: list} do
-      assert_repo_changed(List, -1, fn ->
-        Service.delete_list(list.id)
-        Process.sleep(200)
-      end)
+      Service.delete_list(list.id)
+      assert_receive {LiveviewTodos.TodoTopic, [:lists, :deleted], new_list}
     end
 
     test "lists()", %{list: list} do
