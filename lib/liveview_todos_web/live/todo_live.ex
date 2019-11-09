32be5811eed0457e4ac5b09986eca3dc55627623
Mark Windholtz <mark@agiledna.com>
2019-11-09 10:27:36 -0500

    limbo-commit

--- lib/liveview_todos_web/live/todo_live.ex
+++ lib/liveview_todos_web/live/todo_live.ex
@@ -1,12 +1,15 @@
 defmodule LiveviewTodosWeb.TodoLive do
   use Phoenix.LiveView
   alias LiveviewTodos.TodoApplicationService, as: Service
-  alias LiveviewTodosWeb.TodoView
   alias LiveviewTodos.TodoTopic
+  alias LiveviewTodosWeb.TodoView
+  alias LiveviewTodosWeb.TodoLive.Command
+  alias Phoenix.LiveView.Socket
+
   require Logger
   # --------- LiveView -----------
 
-  def mount(_session, socket) do
+  def mount(_session, %Socket{} = socket) do
     TodoTopic.subscribe()
 
     socket =
@@ -25,27 +28,31 @@ defmodule LiveviewTodosWeb.TodoLive do
 
   # --------- LiveView Events From the User Interface-----------
 
-  def handle_event("create-list", %{"list" => %{"name" => name}}, socket) do
+  def handle_event("create-list", %{"list" => %{"name" => name}}, %Socket{} = socket) do
     todos(socket).create_list(name)
     {:noreply, socket}
   end
 
-  def handle_event("delete-list", %{"list-id" => list_id}, socket) do
+  def handle_event("delete-list", %{"list-id" => list_id}, %Socket{} = socket) do
     todos(socket).delete_list(list_id |> String.to_integer())
     {:noreply, socket}
   end
 
-  def handle_event("add-item", %{"item" => item}, socket) do
+  def handle_event("add-item", %{"item" => item}, %Socket{} = socket) do
     todos(socket).create_item(item)
     {:noreply, socket}
   end
 
-  def handle_event("toggle_done", %{"list-id" => list_id, "item-title" => item_title}, socket) do
+  def handle_event(
+        "toggle_done",
+        %{"list-id" => list_id, "item-title" => item_title},
+        %Socket{} = socket
+      ) do
     todos(socket).toggle_item(list_id, item_title)
     {:noreply, socket}
   end
 
-  def handle_event(event, args, socket) do
+  def handle_event(event, args, %Socket{} = socket) do
     Logger.error("UNHANDED LIVE EVENT: #{event} ===== ARGS: #{args}")
     {:noreply, socket}
   end
@@ -54,51 +61,53 @@ defmodule LiveviewTodosWeb.TodoLive do
 
   @topic LiveviewTodos.TodoTopic
 
-  def handle_info(:load_all, socket) do
-    {:noreply, refresh_lists(socket)}
+  def handle_info(:load_all, %Socket{} = socket) do
+    {:noreply, Command.refresh_lists(socket)}
   end
 
-  def handle_info({@topic, [:todo | _], :error, _}, socket) do
-    {:noreply, refresh_lists(socket)}
+  def handle_info({@topic, [:todo | _], :error, _}, %Socket{} = socket) do
+    {:noreply, Command.refresh_lists(socket)}
   end
 
-  def handle_info({@topic, [:todo | _], _}, socket) do
-    {:noreply, refresh_lists(socket)}
+  def handle_info({@topic, [:todo | _], _}, %Socket{} = socket) do
+    {:noreply, Command.refresh_lists(socket)}
   end
 
-  def handle_info({@topic, [:lists | _], :error, _}, socket) do
-    {:noreply, refresh_lists(socket)}
+  def handle_info({@topic, [:lists | _], :error, _}, %Socket{} = socket) do
+    {:noreply, Command.refresh_lists(socket)}
   end
 
-  def handle_info({@topic, [:lists | _], _}, socket) do
-    {:noreply, refresh_lists(socket)}
+  def handle_info({@topic, [:lists | _], _}, %Socket{} = socket) do
+    {:noreply, Command.refresh_lists(socket)}
   end
 
-  def handle_info(tuple, socket) do
+  def handle_info(tuple, %Socket{} = socket) do
     Logger.error("UNHANDED PUBSUB TUPLE: #{tuple}")
-    {:noreply, refresh_lists(socket)}
+    {:noreply, Command.refresh_lists(socket)}
+  end
+
+  def todos(%Socket{assigns: assigns} = _socket) do
+    Map.fetch!(assigns, :todo_application_service)
   end
 
   # -------  Implementation ---------------
 
-  def refresh_lists(socket) do
-    socket =
+  defmodule Command do
+    def refresh_lists(%Socket{} = socket) do
+      socket =
+        socket
+        |> assign(list_map: %{})
+
+      Service.list_ids()
+      |> Enum.reduce(socket, fn list_id, socket -> refresh_one_list(socket, list_id) end)
+    end
+
+    def refresh_one_list(%Socket{} = socket, list_id) do
+      list = Service.get_list(list_id)
+      mod_map = Map.put(socket.assigns.list_map, list_id, list)
+
       socket
-      |> assign(list_map: %{})
-
-    Service.list_ids()
-    |> Enum.reduce(socket, fn list_id, socket -> refresh_list(socket, list_id) end)
-  end
-
-  def refresh_list(socket, list_id) do
-    list = Service.get_list(list_id)
-    mod_map = Map.put(socket.assigns.list_map, list_id, list)
-
-    socket
-    |> assign(list_map: mod_map)
-  end
-
-  def todos(%{assigns: assigns} = _socket) do
-    Map.fetch!(assigns, :todo_application_service)
+      |> assign(list_map: mod_map)
+    end
   end
 end
