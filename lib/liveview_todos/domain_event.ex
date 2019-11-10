73738c0135ed64d142f8e031b67f08b999cdbac0
Mark Windholtz <mark@agiledna.com>
2019-11-10 18:56:48 -0500

    limbo-commit

--- lib/liveview_todos/domain_event.ex
+++ lib/liveview_todos/domain_event.ex
@@ -1,6 +1,6 @@
 defmodule LiveviewTodos.DomainEvent do
   @enforce_keys [:name, :attrs]
-  defstruct name: :not_set, attrs: []
+  defstruct name: :not_set, attrs: %{}
 
   def new(name, attrs) do
     %LiveviewTodos.DomainEvent{name: name, attrs: attrs}
